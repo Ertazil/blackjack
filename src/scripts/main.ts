ccb0caca9137ee4b1a3e4cbf3eac3b37cf34c4bb
Ertazil <117348711+Ertazil@users.noreply.github.com>
2023-12-16 16:20:12 +0100

    wip

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,18 +1,19 @@
 import { log } from "console";
 
-import { Main, Name, Paquet, Pioche, Value } from "helpers/cartes";
+import { Main, NameMain, Paquet, Value, newPioche } from "helpers/cartes";
 import logger from "utils/logger";
 
 
 const P = Paquet(1);
-
-
 const M = Main(P);
-const N: string[] = [Name(M[0]), Name(M[1])];
+const Scnd = newPioche(P,M);
+const N = NameMain(M);
 const V = Value(M);
 
-logger.info(P);
 
-logger.info(N);
+
+
+logger.info(P);
 logger.info(M);
+logger.info(N);
 logger.info(V);
