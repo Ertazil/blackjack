bed01b4c38e08e6689d2b82ba5d709e1725bad6a
Ertazil <117348711+Ertazil@users.noreply.github.com>
2023-12-26 10:26:04 +0100

    wip

--- src/helpers/partie.ts
+++ src/helpers/partie.ts
@@ -61,6 +61,6 @@ export function BotvsBot(input: number) {  // nombre de joueur
     while(res < 17){
         res = bot.Bot0(paquet, botmain[0]);;
     }
-
+    console.log(botmainvalue);
     return victoire(botmainvalue);
 }
